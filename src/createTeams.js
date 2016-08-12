dd59688a120358fdd6cbeb23589225c8bbf3397f
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-08-11 22:31:17 -0700

    add create teams

--- src/createTeams.js
+++ src/createTeams.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 var _ = require('lodash');
 
 const CreateTeams = React.createClass({
@@ -13,16 +14,19 @@ const CreateTeams = React.createClass({
     //console.log("teamq",teamq);
     let teams = _.chunk(_.shuffle(this.props.students),teamq);
     this.setState({teams : teams});
-    console.log("teams",teams);
+    // console.log("teams",teams);
   },
   render(){
     //console.log("teams",this.state.teams);
-    let teamslist = this.state.teams.map(team=>{
+    let teamslist = this.state.teams.map((team,i)=>{
       let te= team.map(mem=>{
         return (<li key={mem.id}>{mem.name}</li>);
       });
       return (
-        <ul>{te}</ul>
+          <ul>
+            <span>Team-{i+1}</span>
+            {te}
+          </ul>
       );
     });
     return(
